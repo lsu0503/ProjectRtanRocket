ade52811b248eae708d6b36a16e65bb338bbff4f
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-10-17 13:09:09 +0900

    [Develop] Complete Achivement System

--- ProjectRtanRocket/Assets/Scripts/Challenge/AcheivementManager.cs
+++ ProjectRtanRocket/Assets/Scripts/Challenge/AcheivementManager.cs
@@ -12,8 +12,6 @@ public class AchievementManager : MonoBehaviour
     [SerializeField] private AchievementView achievementView;
 
     // 개인 추가 내용
-    [SerializeField] private RocketMovementC movementC;
-    private float[] challengeList = new float[] { 100.0f, 200.0f, 500.0f, 800.0f, 1200.0f, 2000.0f};
     private int challengeAchivedOrder = 0;
 
     private void Awake()
@@ -32,11 +30,11 @@ public class AchievementManager : MonoBehaviour
     // 최고 높이를 달성했을 때 업적 달성 판단, 이벤트 기반으로 설계할 것
     private void CheckAchievement(float height)
     {
-        if (challengeAchivedOrder >= challengeList.Length) return;
+        if (challengeAchivedOrder >= achievements.Length) return;
 
-        if (height >= challengeList[challengeAchivedOrder])
+        if (height >= achievements[challengeAchivedOrder].threshold)
         {
-            Debug.Log($"You Leached {(int)challengeList[challengeAchivedOrder]}m!");
+            achievementView.UnlockAchievement(challengeAchivedOrder);
             challengeAchivedOrder++;
         }
     }
--- ProjectRtanRocket/Assets/Scripts/Challenge/AchievementView.cs
+++ ProjectRtanRocket/Assets/Scripts/Challenge/AchievementView.cs
@@ -7,13 +7,26 @@ public class AchievementView : MonoBehaviour
     [SerializeField] private GameObject achievementSlotPrefab;  // 업적 슬롯 프리팹
     private Dictionary<int, AchievementSlot> achievementSlots = new();
 
+    // 개인 추가 사항
+    [SerializeField] private Transform contentTransform;
+    private int challengeAchivedOrder = 0;
+
     public void CreateAchievementSlots(AchievementSO[] achievements)
     {
         // achievement 데이터에 따라 슬롯을 생성함
+        for (int i = 0; i < achievements.Length; i++)
+        {
+            GameObject tempObj = Instantiate(achievementSlotPrefab, contentTransform);
+            AchievementSlot tempSlot = tempObj.GetComponent<AchievementSlot>();
+            tempSlot.Init(achievements[i]);
+            achievementSlots.Add(i, tempSlot);
+        }
     }
 
-    public void UnlockAchievement(int threshold)
+    // 기존 코드와의 호환성을 위해서 인수의 이름을 임의로 변경하였습니다.
+    public void UnlockAchievement(int order)
     {
         // UI 반영 로직
+        achievementSlots[order].MarkAsChecked();
     }
 }
