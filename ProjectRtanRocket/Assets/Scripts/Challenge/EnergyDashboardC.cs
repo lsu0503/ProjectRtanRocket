99cf20947268a90cf65fd5ebe13fce8f0872b56b
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-10-17 12:05:48 +0900

    [Develop] FuelSystem & UI

--- ProjectRtanRocket/Assets/Scripts/Challenge/EnergyDashboardC.cs
+++ ProjectRtanRocket/Assets/Scripts/Challenge/EnergyDashboardC.cs
@@ -5,9 +5,16 @@ public class EnergyDashboardC : MonoBehaviour
 {
     [SerializeField] private EnergySystemC energySystem;
     [SerializeField] private Image fillBar;
+
     private void Start()
     {
         // 에너지시스템의 에너지 사용에 대해 fillBar가 변경되도록 수정
+        energySystem.OnEnergyChanged += UpdateEnergyGauge;
     }
 
+    public void UpdateEnergyGauge(float curFuel)
+    {
+        float fuelRatio = curFuel / energySystem.MaxFuel;
+        fillBar.fillAmount = fuelRatio;
+    }
 }
