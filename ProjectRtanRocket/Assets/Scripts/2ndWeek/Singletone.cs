675ee12248413b6db2c45e225be5c791cc4fafa8
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-10-21 13:02:41 +0900

    [Develop] Q2 Generic Singleton - Detail 1

---
+++ ProjectRtanRocket/Assets/Scripts/2ndWeek/Singletone.cs
@@ -0,0 +1,41 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject singletonObj;
+                singletonObj = GameObject.Find(typeof(T).Name);
+
+                if (singletonObj != null)
+                {
+                    singletonObj = new GameObject(typeof(T).Name);
+                    instance = singletonObj.AddComponent<T>();
+                }
+
+                else
+                {
+                    instance = singletonObj.GetComponent<T>();
+
+                    if(instance == null)
+                        instance = singletonObj.AddComponent<T>();
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    public void Awake()
+    {
+        // make it as dontdestroyobject        
+    }
+}
+​
