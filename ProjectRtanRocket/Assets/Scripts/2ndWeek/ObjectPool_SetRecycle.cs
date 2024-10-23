86c9cc81864c94de69173c4caa664525a9a4de73
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-10-23 12:55:48 +0900

    [Develop] Q3 Object Pool - Detail 1, 2, 3
    Detail 1: ObjectPool_SetDestroy
    Detail 2: ObjectPool_SetRecycle
    Detail 3: ObjectPool_UnsetDestroy

---
+++ ProjectRtanRocket/Assets/Scripts/2ndWeek/ObjectPool_SetRecycle.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool_SetRecycle : MonoBehaviour
+{
+    private List<GameObject> pool;
+    private const int minSize = 50;
+    private const int maxSize = 300;
+    private int curIndex;
+
+    void Awake()
+    {
+        curIndex = 0;
+
+        pool = new List<GameObject>();
+        for (int i = 0; i < minSize; i++)
+        {
+            pool.Add(CreateObject());
+        }
+    }
+
+    private GameObject CreateObject()
+    {
+        // [요구스펙 1] Create Object
+        GameObject newObj = new GameObject();
+        return newObj;
+    }
+
+    public GameObject GetObject()
+    {
+        // [요구스펙 2] Get Object
+        GameObject returnObj;
+
+        if (curIndex < pool.Count)
+        {
+            returnObj = pool[curIndex];
+            curIndex++;
+        }
+
+        else
+        {
+            returnObj = pool[0];
+            pool.RemoveAt(0);
+            pool.Add(returnObj);
+        }
+        
+        return returnObj;
+    }
+
+    public void ReleaseObject(GameObject obj)
+    {
+        // [요구스펙 3] Release Object
+        int targetIdx = pool.FindIndex(idx => ReferenceEquals(obj, idx));
+        
+        pool.Add(obj);
+        pool.RemoveAt(targetIdx);
+        curIndex--;
+    }
+}
---
+++ ProjectRtanRocket/Assets/Scripts/2ndWeek/ObjectPool_UnsetDestroy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool_UnsetDestroy : MonoBehaviour
+{
+    private List<GameObject> pool;
+    private const int minSize = 0;
+    private const int maxSize = 100;
+    private int curIndex;
+
+    void Awake()
+    {
+        curIndex = 0;
+
+        pool = new List<GameObject>();
+        for (int i = 0; i < minSize; i++)
+        {
+            pool.Add(CreateObject());
+        }
+    }
+
+    private GameObject CreateObject()
+    {
+        // [요구스펙 1] Create Object
+        GameObject newObj = new GameObject();
+        return newObj;
+    }
+
+    public GameObject GetObject()
+    {
+        // [요구스펙 2] Get Object
+        if (curIndex >= pool.Count)
+        {
+            pool.Add (CreateObject());
+        }
+
+        GameObject returnObj = pool[curIndex];
+        curIndex++;
+        return returnObj;
+    }
+
+    public void ReleaseObject(GameObject obj)
+    {
+        // [요구스펙 3] Release Object
+        int targetIdx = pool.FindIndex(idx => ReferenceEquals(obj, idx));
+
+        if (curIndex >= maxSize)
+            Destroy(obj);
+
+        else
+            pool.Add(obj);
+
+        pool.RemoveAt(targetIdx);
+        curIndex--;
+    }
+}
